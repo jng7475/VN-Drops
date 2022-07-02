84a2647bc8441866a877899a05727db6745208e5
Jason Nguyen <trungnt281@gmail.com>
2022-07-02 12:13:55 +0700

    initialize login screen

---
+++ src/screens/login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Button from '../../components/button';
+
+const LoginPage = () => {
+  return (
+    <Text>
+      LoginPage
+      <Button></Button>
+    </Text>
+  );
+};
+
+export default LoginPage;
