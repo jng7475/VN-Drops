141cb4db792d42c799e5188d96b44e195e5000b7
Trung Nguyen <trungthanh.nguyen@team.enouvo.com>
2022-07-02 12:00:17 +0700

    Init project

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import { Text } from 'react-native';
+
+const App = () => {
+  return <Text>Init Project</Text>;
+};
+
+export default App;
