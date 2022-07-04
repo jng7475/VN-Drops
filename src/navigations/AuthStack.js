759871d49478e86b741a2bab419f8cf3593d27d3
Jason Nguyen <trungnt281@gmail.com>
2022-07-04 13:00:32 +0700

    fix AuthStack bug

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/Login';
+import LoginScreen from '../screens/login';
 
 const Stack = createNativeStackNavigator();
 
