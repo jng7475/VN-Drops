c48893320f3faac87159279bd682eb1806fe7c7e
Jason Nguyen <trungnt281@gmail.com>
2022-07-31 15:43:58 +0700

    new homePage

--- src/screens/Home/MainHome/component/CustomButton.js
+++ src/screens/Home/MainHome/component/CustomButton.js
@@ -14,23 +14,31 @@ const windowHeight = Dimensions.get('window').height;
 const CustomButton = props => {
   return (
     <TouchableOpacity style={styles.container}>
-      <Image source={props.imageLink} />
-      <Text>{props.text}</Text>
+      <Image style={{ maxHeight: 40, maxWidth: 40 }} source={props.imageLink} />
+      <View style={{ width: '80%' }}>
+        <Text style={styles.text}>{props.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: (windowWidth * 4) / 10,
-    height: windowHeight / 10,
+    width: (windowWidth * 4.2) / 10,
+    height: (windowHeight * 1.1) / 10,
     borderRadius: 15,
     backgroundColor: '#F5F5F5',
-    elevation: 10,
+    elevation: 15,
     shadowColor: '#000000',
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.5,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 0.04 * windowWidth,
+  },
+  text: {
+    fontFamily: 'RobotoSlab-SemiBold',
+    fontSize: 14,
+    textAlign: 'center',
   },
 });
 export default CustomButton;
--- src/screens/Home/MainHome/index.js
+++ src/screens/Home/MainHome/index.js
@@ -1,21 +1,67 @@
-import { Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
-import React from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+import React, { useState } from 'react';
 import styles from './styles';
 import NewsPieces from './component/NewsPieces';
 import { NewsInfo } from '../../../utilities/newsInfo';
 import CustomButton from './component/CustomButton';
 import { mainButtonData } from '../../../utilities/mainButtonData';
+import { scheduleButtonData } from '../../../utilities/mainButtonData';
+
+const windowWidth = Dimensions.get('window').width - 20;
+const windowHeight = Dimensions.get('window').height;
 
 const MainHomeScreen = ({ navigation }) => {
   const MainButton = mainButtonData.map(value => {
     return <CustomButton text={value.text} imageLink={value.imageLink} />;
   });
+  const NewsPiece = NewsInfo.map(value => {
+    return (
+      <NewsPieces
+        link={value.link}
+        title={value.title}
+        imageLink={value.imageLink}
+      />
+    );
+  });
   return (
-    <View>
-      <Text>fsdg</Text>
-      {/* {MainButton} */}
-      <CustomButton text={'d'} />
-      {MainButton}
+    <View style={styles.container}>
+      <View style={styles.curvedLine} />
+      <View style={styles.top}>
+        <View style={styles.homeTitleWrapper}>
+          <Text style={styles.homeTitleText}>VN Drops</Text>
+          <Text style={styles.homeSloganText}>Giọt máu cho sự sống</Text>
+        </View>
+        <View style={styles.importantNewsWrapper}>
+          <Text style={styles.importantNewsText}>Thông tin quan trọng !!!</Text>
+          <View style={styles.importantNews}>
+            <Text>bla bla</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.mid}>
+        <View style={styles.midLine1}>
+          <CustomButton
+            text={scheduleButtonData.text}
+            imageLink={scheduleButtonData.imageLink}
+          />
+        </View>
+        <View style={styles.midOthers}>{MainButton}</View>
+      </View>
+      <View style={styles.bottom}>
+        <Text style={styles.newsText}>Tin tức</Text>
+        <ScrollView
+          horizontal={true}
+          style={{ marginTop: '20%', width: windowWidth * 5 }}>
+          {NewsPiece}
+        </ScrollView>
+      </View>
     </View>
   );
 };
